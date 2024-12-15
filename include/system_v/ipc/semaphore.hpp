08493ef17af594219bdcd377d92bfa64a390a0dc
Tomáš Petříček <tomaspetricek@Tomas--MacBook-Air.local>
2024-12-15 12:15:08 +0800

    create system v ipc headers

---
+++ include/system_v/ipc/semaphore.hpp
@@ -0,0 +1,13 @@
+#ifndef SYSTEM_V_IPC_SEMAPHORE_HPP
+#define SYSTEM_V_IPC_SEMAPHORE_HPP
+
+#include <cassert>
+#include <expected>
+
+
+namespace system_v::ipc
+{
+    class semaphore;
+}
+
+#endif // SYSTEM_V_IPC_SEMAPHORE_HPP
---
+++ include/system_v/ipc/shared_memory.hpp
@@ -0,0 +1,13 @@
+#ifndef SYSTEM_V_IPC_SHARED_MEMORY_HPP
+#define SYSTEM_V_IPC_SHARED_MEMORY_HPP
+
+#include <cassert>
+#include <expected>
+
+
+namespace system_v::ipc
+{
+    class shared_memory;
+}
+
+#endif // SYSTEM_IPC_V_SHARED_MEMORY_HPP
