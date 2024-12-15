ee70834e60a04144000129daa78c94a8d628f95c
Tomáš Petříček <tomaspetricek@Tomas--MacBook-Air.local>
2024-12-15 20:59:01 +0800

    protect base constructor

--- include/unix/posix/ipc/primitive.hpp
+++ include/unix/posix/ipc/primitive.hpp
@@ -1,20 +1,20 @@
 #ifndef UNIX_POSIX_IPC_PRIMITIVE_HPP
 #define UNIX_POSIX_IPC_PRIMITIVE_HPP
 
-
 namespace unix::posix::ipc
 {
     class primitive
     {
-    public:
+    protected:
         explicit primitive() noexcept = default;
-        
+
+    public:
         primitive(const primitive &other) = delete;
         primitive &operator=(const primitive &other) = delete;
 
         primitive(primitive &&other) noexcept = delete;
         primitive &operator=(primitive &&other) noexcept = delete;
     };
-}
+} // namespace unix::posix::ipc
 
 #endif // UNIX_POSIX_IPC_PRIMITIVE_HPP
