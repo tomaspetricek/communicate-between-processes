ff0d0a86cfb61632fe854f44365368755eeeeae0
Tomáš Petříček <tomaspetricek@Tomas--MacBook-Air.local>
2024-12-28 22:19:06 +0700

    extract process creation

---
+++ apps/consumer_producer/include/buffering/process_creation.hpp
@@ -0,0 +1,63 @@
+#ifndef BUFFERING_PROCESS_CREATION_HPP
+#define BUFFERING_PROCESS_CREATION_HPP
+
+#include <array>
+#include <cassert>
+#include <cstddef>
+#include <print>
+
+#include "unix/error_code.hpp"
+#include "unix/process.hpp"
+
+#include "buffering/process_info.hpp"
+
+namespace buffering
+{
+    buffering::process_info create_child_processes(std::size_t child_producer_count,
+                                               std::size_t child_consumer_count) noexcept
+{
+    constexpr std::size_t process_counts_size{2};
+    using process_counts_t = std::array<std::size_t, process_counts_size>;
+    static_assert(process_counts_size == std::size_t{2});
+    const auto process_counts =
+        process_counts_t{child_producer_count, child_consumer_count};
+    process_info info;
+    info.is_child = true;
+    std::size_t i{0};
+    assert(info.is_producer == false);
+
+    for (std::size_t c{0}; c < process_counts.size(); ++c)
+    {
+        info.is_producer = c;
+
+        for (i = 0; i < process_counts[c]; ++i)
+        {
+            const auto process_created = unix::create_process();
+
+            if (!process_created)
+            {
+                std::println("failed to create child process due to: {}",
+                             unix::to_string(process_created.error()).data());
+                break;
+            }
+            info.created_process_count++;
+
+            if (!unix::is_child_process(process_created.value()))
+            {
+                std::println("created child process with id: {}",
+                             process_created.value());
+            }
+            else
+            {
+                return info;
+            }
+        }
+    }
+    info.is_child = false;
+    info.is_producer = true;
+    info.group_id = i;
+    return info;
+}
+}
+
+#endif // BUFFERING_PROCESS_CREATION_HPP
--- apps/consumer_producer/src/main.cpp
+++ apps/consumer_producer/src/main.cpp
@@ -22,6 +22,7 @@
 #include "buffering/occupation.hpp"
 #include "buffering/process_info.hpp"
 #include "buffering/processor.hpp"
+#include "buffering/process_creation.hpp"
 #include "buffering/role.hpp"
 
 template <string_literal ResourceName, class Resource>
@@ -48,52 +49,6 @@ template <string_literal ResourceName, class Resource>
 using resource_destroyer_t =
     std::unique_ptr<Resource,
                     unix::deleter<destroy_resource<ResourceName, Resource>>>;
-
-buffering::process_info create_child_processes(std::size_t child_producer_count,
-                                               std::size_t child_consumer_count) noexcept
-{
-    constexpr std::size_t process_counts_size{2};
-    using process_counts_t = std::array<std::size_t, process_counts_size>;
-    static_assert(process_counts_size == std::size_t{2});
-    const auto process_counts =
-        process_counts_t{child_producer_count, child_consumer_count};
-    buffering::process_info info;
-    info.is_child = true;
-    std::size_t i{0};
-    assert(info.is_producer == false);
-
-    for (std::size_t c{0}; c < process_counts.size(); ++c)
-    {
-        info.is_producer = c;
-
-        for (i = 0; i < process_counts[c]; ++i)
-        {
-            const auto process_created = unix::create_process();
-
-            if (!process_created)
-            {
-                std::println("failed to create child process due to: {}",
-                             unix::to_string(process_created.error()).data());
-                break;
-            }
-            info.created_process_count++;
-
-            if (!unix::is_child_process(process_created.value()))
-            {
-                std::println("created child process with id: {}",
-                             process_created.value());
-            }
-            else
-            {
-                return info;
-            }
-        }
-    }
-    info.is_child = false;
-    info.is_producer = true;
-    info.group_id = i;
-    return info;
-}
 
 struct shared_data
 {
@@ -176,7 +131,7 @@ int main(int, char **)
     }
     std::println("semaphores initialized");
     const auto info =
-        create_child_processes(child_producer_count, child_consumer_count);
+        buffering::create_child_processes(child_producer_count, child_consumer_count);
 
     if (!info.is_child)
     {
