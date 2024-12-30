1d69bce8822d3edf4c143efad1080235f5627022
Tomáš Petříček <tomaspetricek@Tomas--MacBook-Air.local>
2024-12-30 13:49:45 +0700

    enable interruption signal

--- libs/unix/include/unix/signal.hpp
+++ libs/unix/include/unix/signal.hpp
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <expected>
 #include <signal.h>
+#include <optional>
 
 #include "unix/error_code.hpp"
 #include "unix/process.hpp"
@@ -11,7 +12,12 @@
 
 namespace unix
 {
-    std::expected<void, error_code> send_termination_signal(process_id_t pid, int signal) noexcept
+    using signal_handler_t = void (*)(int);
+    using signal_t = int;
+
+    constexpr process_id_t group_process_id{0};
+
+    std::expected<void, error_code> send_termination_signal(process_id_t pid, signal_t signal) noexcept
     {
         const auto ret = kill(pid, signal);
 
@@ -30,7 +36,7 @@ namespace unix
 
     std::expected<void, error_code> request_process_group_termination() noexcept
     {
-        return request_process_termination(process_id_t{0});
+        return request_process_termination(group_process_id);
     }
 
     std::expected<void, error_code> request_all_accessible_process_termination() noexcept
@@ -45,12 +51,38 @@ namespace unix
 
     std::expected<void, error_code> force_process_group_termination() noexcept
     {
-        return force_process_termination(process_id_t{0});
+        return force_process_termination(group_process_id);
     }
 
     std::expected<void, error_code> force_all_accessible_process_termination() noexcept
     {
         return force_process_termination(process_id_t{-1});
+    }
+
+    std::expected<void, error_code> interrupt_process(process_id_t pid) noexcept
+    {
+        return send_termination_signal(pid, SIGINT);
+    }
+
+    std::expected<void, error_code> interrupt_process_group() noexcept
+    {
+        return interrupt_process(group_process_id);
+    }
+
+    std::optional<signal_handler_t> register_signal_handler(signal_t sig, signal_handler_t handler) noexcept
+    {
+        const auto ret = signal(sig, handler);
+
+        if (ret == SIG_ERR)
+        {
+            return std::nullopt;
+        }
+        return ret;
+    }
+
+    std::optional<signal_handler_t> register_interruption_handler(signal_handler_t handler) noexcept
+    {
+        return register_signal_handler(SIGINT, handler);
     }
 }
 
