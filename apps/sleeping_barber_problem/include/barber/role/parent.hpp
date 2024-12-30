a55014fc3f1a34c010f5e90717cc3b4f5516b623
Tomáš Petříček <tomaspetricek@Tomas--MacBook-Air.local>
2024-12-30 10:16:15 +0700

    fix file format

--- apps/sleeping_barber_problem/include/barber/role/parent.hpp
+++ apps/sleeping_barber_problem/include/barber/role/parent.hpp
@@ -11,54 +11,55 @@
 
 #include "barber/process_info.hpp"
 
-namespace barber::role {
-    // ToDo: avoid copying
-process_info
-create_child_processes(std::size_t barber_count,
-                       std::size_t customer_generator_count) noexcept
+namespace barber::role
 {
-    constexpr std::size_t process_counts_size{2};
-    using process_counts_t = std::array<std::size_t, process_counts_size>;
-    static_assert(process_counts_size == std::size_t{2});
-    const auto process_counts =
-        process_counts_t{customer_generator_count, barber_count};
-    process_info info;
-    info.is_child = true;
-    std::size_t i{0};
-    assert(info.is_barber == false);
-
-    for (std::size_t c{0}; c < process_counts.size(); ++c)
+    // ToDo: avoid copying
+    process_info
+    create_child_processes(std::size_t barber_count,
+                           std::size_t customer_generator_count) noexcept
     {
-        info.is_barber = c;
+        constexpr std::size_t process_counts_size{2};
+        using process_counts_t = std::array<std::size_t, process_counts_size>;
+        static_assert(process_counts_size == std::size_t{2});
+        const auto process_counts =
+            process_counts_t{customer_generator_count, barber_count};
+        process_info info;
+        info.is_child = true;
+        std::size_t i{0};
+        assert(info.is_barber == false);
 
-        for (i = 0; i < process_counts[c]; ++i)
+        for (std::size_t c{0}; c < process_counts.size(); ++c)
         {
-            const auto process_created = unix::create_process();
+            info.is_barber = c;
 
-            if (!process_created)
+            for (i = 0; i < process_counts[c]; ++i)
             {
-                std::println("failed to create child process due to: {}",
-                             unix::to_string(process_created.error()).data());
-                break;
-            }
-            info.created_process_count++;
+                const auto process_created = unix::create_process();
 
-            if (!unix::is_child_process(process_created.value()))
-            {
-                std::println("created child process with id: {}",
-                             process_created.value());
-            }
-            else
-            {
-                return info;
+                if (!process_created)
+                {
+                    std::println("failed to create child process due to: {}",
+                                 unix::to_string(process_created.error()).data());
+                    break;
+                }
+                info.created_process_count++;
+
+                if (!unix::is_child_process(process_created.value()))
+                {
+                    std::println("created child process with id: {}",
+                                 process_created.value());
+                }
+                else
+                {
+                    return info;
+                }
             }
         }
+        info.is_child = false;
+        info.is_barber = false;
+        info.group_id = 0;
+        return info;
     }
-    info.is_child = false;
-    info.is_barber = false;
-    info.group_id = 0;
-    return info;
-}
 }
 
 #endif // BARBER_ROLE_PARENT_HPP
--- apps/sleeping_barber_problem/include/barber/shared_data.hpp
+++ apps/sleeping_barber_problem/include/barber/shared_data.hpp
@@ -9,14 +9,14 @@
 
 namespace barber
 {
-struct shared_data
-{
-    std::atomic<bool> shop_closed{false};
-    std::atomic<customer_t> next_customer_id{0};
-    std::atomic<std::int32_t> served_customers{0};
-    std::atomic<std::int32_t> refused_customers{0};
-    customer_queue_t customer_queue;
-};
+    struct shared_data
+    {
+        std::atomic<bool> shop_closed{false};
+        std::atomic<customer_t> next_customer_id{0};
+        std::atomic<std::int32_t> served_customers{0};
+        std::atomic<std::int32_t> refused_customers{0};
+        customer_queue_t customer_queue;
+    };
 } // namespace barber
 
 #endif // BARBER_SHARED_DATA_HPP
