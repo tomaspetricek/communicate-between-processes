e48ca8e2fc178f7ee79a510d4871ff80e0129e6f
Tomáš Petříček <tomaspetricek@Tomas--MacBook-Air.local>
2025-01-01 16:51:02 +0100

    improve unit test

--- tests/lock_free_tests/test_message_ring_buffer.cpp
+++ tests/lock_free_tests/test_message_ring_buffer.cpp
@@ -8,43 +8,61 @@
 
 #include "lock_free/message_ring_buffer.hpp"
 
+template <std::size_t MessageCount>
+constexpr std::size_t compute_buffer_min_capacity(
+    const std::array<std::string_view, MessageCount> &messages) noexcept
+{
+    std::size_t res{MessageCount * sizeof(std::size_t)};
+
+    for (const auto &msg : messages)
+    {
+        res += msg.size();
+    }
+    return res + 1;
+}
+
+using message_view = std::span<const char>;
+
+void are_same(const message_view &actual, const message_view &expect) noexcept
+{
+    ASSERT_EQ(expect.size(), actual.size());
+
+    for (const auto &[e, a] : std::views::zip(expect, actual))
+    {
+        ASSERT_EQ(e, a);
+    }
+}
+
 TEST(MessageRingBuffer, TestPushingPoppingMessages)
 {
-    constexpr std::string_view in_msg1{"pie apple pen"};
-    constexpr std::size_t it_count{10}, message_count{3};
-    constexpr std::size_t buffer_size{
-        (message_count * (sizeof(std::size_t) + in_msg1.size())) + 1};
-    lock_free::message_ring_buffer<buffer_size> queue;
-    std::vector<char> out_msg1;
-    out_msg1.reserve(in_msg1.size());
+    constexpr std::size_t message_count = 3;
+    constexpr auto input_messages =
+        std::array<std::string_view, message_count>{"pie", "apple", "pie"};
+    constexpr auto buffer_capacity = compute_buffer_min_capacity(input_messages);
+    constexpr std::size_t it_count{10};
+    lock_free::message_ring_buffer<buffer_capacity> queue;
+    std::vector<char> out_msg;
+    out_msg.reserve(10);
 
     for (std::size_t i{0}; i < it_count; ++i)
     {
         assert(queue.empty());
 
-        for (std::size_t m{0}; m < message_count; ++m)
+        for (const auto &msg : input_messages)
         {
             assert(!queue.full());
-            assert(queue.try_push(in_msg1));
+            assert(queue.try_push(msg));
             assert(!queue.empty());
         }
         assert(queue.full());
 
-        for (std::size_t m{0}; m < message_count; ++m)
+        for (const auto &in_msg : input_messages)
         {
             assert(!queue.empty());
-            assert(queue.try_pop(out_msg1));
+            assert(queue.try_pop(out_msg));
             assert(!queue.full());
-            const auto expect = std::span<const char>(in_msg1.data(), in_msg1.size());
-            const auto actual =
-                std::span<const char>(out_msg1.data(), out_msg1.size());
-
-            ASSERT_EQ(expect.size(), actual.size());
-
-            for (const auto &[e, a] : std::views::zip(expect, actual))
-            {
-                ASSERT_EQ(e, a);
-            }
+            are_same(message_view(in_msg.data(), in_msg.size()),
+                     message_view(out_msg.data(), out_msg.size()));
         }
         assert(queue.empty());
     }
