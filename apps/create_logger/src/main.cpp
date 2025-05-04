d370704d06f0a6dcfaa6e2c788c3922ceb137014
Tomáš Petříček <tomaspetricek@Tomas--MacBook-Air.local>
2025-05-04 09:54:24 +0200

    format log

--- apps/create_logger/src/main.cpp
+++ apps/create_logger/src/main.cpp
@@ -41,17 +41,25 @@ namespace log
         static log::logger curr_logger{};
 
         template <class First, class... Rest>
-        bool create_log(const std::string_view &format, const First &first,
+        bool create_log(const token_t &format, const First &first,
                         Rest &&...rest) noexcept
         {
-            const auto token = std::to_string(first);
+            std::size_t index{0};
 
-            if (!curr_logger.log(token_t{token.data(), token.size()}))
+            for (; index != format.size() && format[index] != '{'; index++)
+            {
+            }
+            if (index == format.size())
             {
                 return false;
             }
-            // ToDo: remove
-            if (!curr_logger.log(token_t{" "}))
+            if (!curr_logger.log(token_t{format.data(), index}))
+            {
+                return false;
+            }
+            const auto token = std::to_string(first);
+
+            if (!curr_logger.log(token_t{token.data(), token.size()}))
             {
                 return false;
             }
@@ -61,7 +69,19 @@ namespace log
             }
             else
             {
-                return create_log(format, std::forward<Rest>(rest)...);
+                index++;
+
+                for (; index != format.size() && format[index] != '}'; index++)
+                {
+                }
+                if (index == format.size())
+                {
+                    return false;
+                }
+                const auto format_written_count{index + 1};
+                return create_log(token_t{format.data() + format_written_count,
+                                          format.size() - format_written_count},
+                                  std::forward<Rest>(rest)...);
             }
             return true;
         }
@@ -74,14 +94,17 @@ namespace log
             {
                 return true;
             }
-            // ToDo: use format together with args
-            if (!curr_logger.log(token_t{format.data(), format.size()}))
+            if constexpr (sizeof...(args) == std::size_t{0})
             {
-                return false;
+                if (!curr_logger.log(token_t{format.data(), format.size()}))
+                {
+                    return false;
+                }
             }
-            if constexpr (sizeof...(args) != std::size_t{0})
+            else
             {
-                if (!create_log(format, std::forward<Args>(args)...))
+                if (!create_log(token_t{format.data(), format.size()},
+                                std::forward<Args>(args)...))
                 {
                     return false;
                 }
@@ -130,7 +153,8 @@ namespace log
 int main(int, char **)
 {
     std::println("create logger");
-    log::info("hello world");
+    log::info("provide no arguments");
+    log::info("provide single argument: {}", 42);
     log::info("provide multiple multiple arguents: first: {}, second: {}", 10,
               2.F);
 }
