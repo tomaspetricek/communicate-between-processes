761cf80e8f34c35d4427f8bdb864668c49dd1851
Tomáš Petříček <tomaspetricek@Tomas--MacBook-Air.local>
2025-05-08 14:13:03 +0200

    open in memory database

--- apps/use_sqlite/src/main.cpp
+++ apps/use_sqlite/src/main.cpp
@@ -1,6 +1,19 @@
 #include <print>
+#include <sqlite3.h>
 
 int main(int, char **)
 {
     std::println("use sqlite database");
+
+    sqlite3 *db{nullptr};
+    if (sqlite3_open(":memory:", &db) == SQLITE_OK)
+    {
+        std::println("SQLite opened in memory");
+        sqlite3_close(db);
+    }
+    else
+    {
+        std::println("failed to open SQLite DB\n");
+    }
+    return 0;
 }
