14b111e02677f7cba6d74f6b9c762bebc8ed02b4
Tomáš Petříček <tomaspetricek@Tomas--MacBook-Air.local>
2025-05-11 11:58:56 +0200

    create database

--- apps/use_sqlite/src/main.cpp
+++ apps/use_sqlite/src/main.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <cstdint>
+#include <filesystem>
 #include <optional>
 #include <print>
 #include <sqlite3.h>
@@ -63,7 +64,7 @@ public:
     }
 
     static std::optional<prepared_statement> create(sqlite3 *database_handle,
-                                           const char *query) noexcept
+                                                    const char *query) noexcept
     {
         assert(database_handle != nullptr);
         sqlite3_stmt *stmt{nullptr};
@@ -78,13 +79,26 @@ public:
         return prepared_statement{stmt};
     }
 
-    bool set_value(int column, int value) noexcept
+    bool set_value(int index, int value) noexcept
     {
-        assert(column >= 1);
-        if (sqlite3_bind_int(stmt_, column, value) != SQLITE_OK)
+        assert(index >= 0);
+
+        if (sqlite3_bind_int(stmt_, index + 1, value) != SQLITE_OK)
         {
-            std::println(stderr, "failed to set value: {} to column: {}", value,
-                         column);
+            std::println(stderr, "failed to set value: {} at index: {}", value,
+                         index);
+            return false;
+        }
+        return true;
+    }
+
+    bool set_value(int index, const std::string_view &value) noexcept
+    {
+        if (sqlite3_bind_text(stmt_, index, value.data(), -1, SQLITE_TRANSIENT) !=
+            SQLITE_OK)
+        {
+            std::println(stderr, "failed to set value: {} at index: {}", value.data(),
+                         index);
             return false;
         }
         return true;
@@ -96,16 +110,24 @@ public:
         value = sqlite3_column_int(stmt_, 0);
     }
 
+    bool destroy() noexcept
+    {
+        if (sqlite3_finalize(stmt_) != SQLITE_OK)
+        {
+            std::println(stderr, "failed to finalize statement");
+            return false;
+        }
+        stmt_ = nullptr;
+        return true;
+    }
+
     ~prepared_statement() noexcept
     {
         if (stmt_ == nullptr)
         {
             return;
         }
-        if (sqlite3_finalize(stmt_) != SQLITE_OK)
-        {
-            std::println(stderr, "failed to finalize statement");
-        }
+        destroy();
     }
 };
 
@@ -115,7 +137,8 @@ class person_writer
         "INSERT INTO people (name, age) VALUES (?, ?);";
     prepared_statement stmt_;
 
-    explicit person_writer(prepared_statement &&stmt) noexcept : stmt_{std::move(stmt)} {}
+    explicit person_writer(prepared_statement &&stmt) noexcept
+        : stmt_{std::move(stmt)} {}
 
 public:
     static std::optional<person_writer>
@@ -126,19 +149,52 @@ public:
 
         if (!stmt.has_value())
         {
-            std::println(stderr, "failed to create prepared_statement");
+            std::println(stderr, "failed to create prepared statement");
             return std::nullopt;
         }
         return person_writer(std::move(stmt.value()));
     }
 
-    bool write(const person_t &person) { return true; }
+    bool write(const person_t &person) noexcept
+    {
+        int index{0};
+
+        if (!stmt_.set_value(index++, person.age))
+        {
+            std::println("failed to set age: {}", person.age);
+            return false;
+        }
+        if (!stmt_.set_value(index++, person.name.data()))
+        {
+            std::println("failed to set name: {}", person.name);
+            return false;
+        }
+        return true;
+    }
+
+    bool destroy() noexcept { return stmt_.destroy(); }
 };
+
+bool execute_script(sqlite3 *database_handle,
+                    const std::string_view &script) noexcept
+{
+    char *error_message = 0;
+
+    if (sqlite3_exec(database_handle, script.data(), 0, 0, &error_message) !=
+        SQLITE_OK)
+    {
+        std::println(stderr, "failed to run script: \ndue to: {}", script.data(),
+                     error_message);
+        sqlite3_free(error_message);
+        return false;
+    }
+    return true;
+}
 
 class sql_database
 {
-    sqlite3 *disk_database_handle_{nullptr}, *in_memory_database_handle_{nullptr};
     person_writer person_writer_;
+    sqlite3 *disk_database_handle_{nullptr}, *in_memory_database_handle_{nullptr};
 
     explicit sql_database(sqlite3 *disk_database_handle_,
                           sqlite3 *in_memory_database_handle,
@@ -146,6 +202,23 @@ class sql_database
         : disk_database_handle_{disk_database_handle_},
           in_memory_database_handle_{in_memory_database_handle},
           person_writer_{std::move(person_writer)} {}
+
+    static bool
+    load_into_memory_impl(sqlite3 *disk_database_handle,
+                          sqlite3 *in_memory_database_handle) noexcept
+    {
+        assert(disk_database_handle != nullptr);
+        assert(in_memory_database_handle != nullptr);
+
+        if (!copy_content(disk_database_handle, in_memory_database_handle))
+        {
+            std::println(
+                stderr,
+                "failed to copy content of disk database to in memory database");
+            return true;
+        }
+        return true;
+    }
 
 public:
     // forbid copying
@@ -180,6 +253,8 @@ public:
             sqlite3_close(disk_database_handle);
             return std::nullopt;
         }
+        std::println("disk database opened");
+
         if (sqlite3_open(":memory:", &in_memory_database_handle) != SQLITE_OK)
         {
             std::println(stderr, "failed to open in memory database due to: {}",
@@ -188,6 +263,16 @@ public:
             sqlite3_close(in_memory_database_handle);
             return std::nullopt;
         }
+        std::println("in memory database opened");
+        std::println("loading content of disk database into in memory database");
+
+        if (!load_into_memory_impl(disk_database_handle,
+                                   in_memory_database_handle))
+        {
+            return std::nullopt;
+        }
+        std::println("content loaded into in memory database");
+
         auto person_writer_created =
             person_writer::create(in_memory_database_handle);
 
@@ -198,20 +283,15 @@ public:
             sqlite3_close(in_memory_database_handle);
             return std::nullopt;
         }
+        std::println("person writer created");
         return sql_database(disk_database_handle, in_memory_database_handle,
                             std::move(person_writer_created.value()));
     }
 
     bool load_into_memory() noexcept
     {
-        if (!copy_content(disk_database_handle_, in_memory_database_handle_))
-        {
-            std::println(
-                stderr,
-                "failed to copy content of disk database to in memory database");
-            return true;
-        }
-        return true;
+        return load_into_memory_impl(disk_database_handle_,
+                                     in_memory_database_handle_);
     }
 
     bool flush_to_disk() const
@@ -239,6 +319,8 @@ public:
 
     ~sql_database() noexcept
     {
+        person_writer_.destroy();
+
         if (disk_database_handle_ == nullptr)
         {
             assert(in_memory_database_handle_ == nullptr);
@@ -257,10 +339,76 @@ public:
     }
 };
 
+static constexpr std::string_view create_script{
+    "CREATE TABLE IF NOT EXISTS people ("
+    "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+    "name TEXT NOT NULL,"
+    "age INTEGER NOT NULL);"};
+
+bool close_database(sqlite3 *database_handle,
+                    const std::filesystem::path &database_path) noexcept
+{
+    assert(database_handle != nullptr);
+
+    if (sqlite3_close(database_handle) != SQLITE_OK)
+    {
+        std::println(stderr, "failed to close database: {}",
+                     database_path.string().data());
+        return false;
+    }
+    return true;
+}
+
+bool create_database(const std::filesystem::path &destination,
+                     const std::string_view &create_script) noexcept
+{
+    sqlite3 *database_handle{nullptr};
+
+    if (sqlite3_open(destination.string().data(), &database_handle) !=
+        SQLITE_OK)
+    {
+        std::println(stderr, "failed to open database: {}",
+                     sqlite3_errmsg(database_handle));
+        close_database(database_handle, destination);
+        return false;
+    }
+    std::println("executing create script: \n{}", create_script.data());
+
+    if (!execute_script(database_handle, create_script))
+    {
+        std::println(stderr, "failed to execute create script");
+        close_database(database_handle, destination);
+        return false;
+    }
+    close_database(database_handle, destination);
+    return true;
+}
+
 int main(int, char **)
 {
     std::println("use sqlite database");
-    auto database = sql_database::open("database.db");
+
+    std::error_code error_code;
+    const auto current_path = std::filesystem::current_path(error_code);
+
+    if (error_code)
+    {
+        std::println(stderr, "failed to get current path due to: {}",
+                     error_code.message());
+        return EXIT_FAILURE;
+    }
+    std::println("current path {}", current_path.string().data());
+    const auto database_path = current_path / "database.db";
+
+    if (!create_database(database_path, create_script))
+    {
+        std::println("failed to create database: {}",
+                     database_path.string().data());
+        return EXIT_FAILURE;
+    }
+    std::println("database created: {}", database_path.string().data());
+
+    auto database = sql_database::open(database_path.string().data());
 
     if (!database.has_value())
     {
@@ -270,6 +418,14 @@ int main(int, char **)
     if (!database.value().load_into_memory())
     {
         std::println(stderr, "failed to load data into memory");
+        return EXIT_FAILURE;
+    }
+    const person_t person{"Tom", 26};
+
+    if (!database.value().write_person(person))
+    {
+        std::println(stderr, "failed to write person: name: {}, age: {}",
+                     person.name, person.age);
         return EXIT_FAILURE;
     }
     if (!database.value().flush_to_disk())
