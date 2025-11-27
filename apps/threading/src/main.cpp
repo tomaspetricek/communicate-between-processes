d4eb3bf152223f7379641085a7df3990a0a1a893
Tomas Petricek <tomaspetricek98@gmail.com>
2025-11-27 10:43:08 +0100

    parallel accumulate

--- apps/threading/src/main.cpp
+++ apps/threading/src/main.cpp
@@ -1,6 +1,8 @@
 #include <memory>
 #include <print>
 #include <thread>
+#include <algorithm>
+#include <numeric>
 
 class background_task
 {
@@ -113,7 +115,7 @@ public:
         thread_ = std::move(other.thread_);
         return *this;
     }
-    
+
     ~joining_thread() noexcept
     {
         if (joinable())
@@ -137,6 +139,51 @@ public:
     const std::thread &as_thread() const noexcept { return thread_; }
 };
 
+template <class Iterator, class T>
+struct accumulate_block
+{
+    void operator()(Iterator first, Iterator last, T &result)
+    {
+        result = std::accumulate(first, last, result);
+    }
+};
+
+template <class Iterator, class T>
+T parallel_accumulate(Iterator first, Iterator last, T init)
+{
+    const auto length = std::distance(first, last);
+
+    if (!length)
+    {
+        return init;
+    }
+    const unsigned long min_per_thread = 25;
+    const unsigned long max_threads = (length + min_per_thread - 1) / min_per_thread;
+    const unsigned long hardware_threads = std::thread::hardware_concurrency();
+
+    const unsigned long num_threads = std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
+    const unsigned long block_size = length / num_threads;
+    std::vector<T> results(num_threads);
+    std::vector<std::thread> threads(num_threads - 1);
+
+    Iterator block_start = first;
+
+    for (unsigned long i = 0; i < threads.size(); i++)
+    {
+        Iterator block_end = block_start;
+        std::advance(block_end, block_size);
+        threads[i] = std::thread(accumulate_block<Iterator, T>{}, block_start, block_end, std::ref(results[i]));
+        block_start = block_end;
+    }
+    accumulate_block<Iterator, T>{}(block_start, last, results.back());
+
+    for (auto &thread : threads)
+    {
+        thread.join();
+    }
+    return std::accumulate(results.cbegin(), results.cend(), init);
+}
+
 int main()
 {
     std::thread task([]()
@@ -158,4 +205,13 @@ int main()
 
     auto obj = std::make_unique<big_object>();
     scoped_thread task5(std::thread(process_big_object, std::move(obj)));
+
+    std::vector<int> vals(1'000);
+
+    for (std::size_t i{0}; i < vals.size(); i++)
+    {
+        vals[i] = static_cast<int>(i);
+    }
+    const auto res = parallel_accumulate(vals.cbegin(), vals.cend(), 0L);
+    std::println("sum: {}", res);
 }
